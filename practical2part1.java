0ea50fbcbe5403a0b83913b2cb1034a73479893f
kaveeshayomal <IT23372962@my.sliit.lk>
2024-07-23 14:25:28 +0530

    first

---
+++ practical2part1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class practical2part1{
+	public static void main(String[] args){
+		Scanner scanner=new Scanner(System.in);
+		StringBuilder stringBuilder = new StringBuilder();
+		System.out.println("Enter your first name:");
+		String str1=scanner.nextLine();
+
+		System.out.println("Enter your middle name:");
+		String str2=scanner.nextLine();
+
+		System.out.println("Enter your last name:");
+		String str3=scanner.nextLine();
+		
+		stringBuilder.append(str1);
+		stringBuilder.append(" " + str2);
+		stringBuilder.append(" " + str3);
+
+		String result= stringBuilder.toString();
+
+		System.out.println("" +result);
+
+	
+	
+	
+	
+	
+	
+	}
+
+
+}
